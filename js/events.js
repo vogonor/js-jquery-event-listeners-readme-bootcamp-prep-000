56a10de992d49088eec8258bc779d9dba5ac53c4
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-25 22:19:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,37 @@
-//define functions here
+function getIt() {
+  
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+
+  $("img").on("load", function() {
+    
+    $("img").addClass("tasty")
+    
+  });
+}
+
+function pressIt() {
+  
+  $("form").on("keydown", function(key) {
+    
+     if (key.which == 71){
+       $("input:first").submit()
+        alert("You have pressed the G key");
+      }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+  
+
 
 $(document).ready(function(){
 
