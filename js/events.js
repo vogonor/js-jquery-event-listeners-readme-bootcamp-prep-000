6068df1e9e149a71edc833475dcd821e74e7322c
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-25 20:52:15 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,40 @@
-//define functions here
+function getIt() {
+  
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+
+  $("img").on("load", function() {
+    
+    $("img").addClass("tasty")
+    
+  });
+}
+
+function pressIt() {
+  
+  $("form").on("keyDown", function(key) {
+    
+     if (key.which == 71){
+        alert("You have pressed the G key");
+      }
+  })
+}
+
+function submitI(){
+  $("form").on("submit", function(e){
+     if ($( "input").first().val() === 71) {
+    e.alert('your form is going to be submitted now.');
+    return;
+  } else {
+  alert("you entered the wrong value");
+  return;
+  }
+  })
+}
 
 $(document).ready(function(){
 
